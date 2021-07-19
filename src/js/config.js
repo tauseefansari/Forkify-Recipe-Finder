79177b0e01bcf629ef492cf7c7ff1b7ecab2546c
Tauseef Ansari <tauseeftanvir@gmail.com>
2021-07-19 19:50:18 +0530

    Initial Upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4cefbe41-750d-4be5-99e8-9933c4723fde';
+export const MODAL_CLOSE_SEC = 2.5;
